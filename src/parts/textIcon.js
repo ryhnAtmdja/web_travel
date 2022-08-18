71eb80f942bfb97aa23baa8626b7bc8f3fc1e794
rynhnAtmdja <dealguein123@gmail.com>
2022-08-18 15:11:54 +0700

    fixing hover effect in brand text

---
+++ src/parts/textIcon.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "components/Button";
+
+const TextIcon = () => {
+  return (
+    <Button className="btn-logo-txt" type="link" href="/">
+      Stay<span className="text-gray-900">cation.</span>
+    </Button>
+  );
+};
+
+export default TextIcon;
